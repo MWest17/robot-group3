15a1ebd420372dacca61b7525ba98524bac415d7
M West <mwest49@usf.edu>
2024-10-15 16:08:33 -0400

    Fixed bugs

--- SR04.cpp
+++ SR04.cpp
@@ -1,10 +1,12 @@
 #include "SR04.hpp"
 
+#include "wiring_pulse.c"
+
 #define TRIGGER_DELAY 10
 #define SOUND_CONSTANT 0.017 //microseconds per inch 
 
-SR04class::SR04class(uint8_t vcc, uint8_t echo) {
-        this->vcc = trig;
+SR04class::SR04class(uint8_t trig, uint8_t echo) {
+        this->trig = trig;
         this->echo = echo;    
         pinMode(trig, OUTPUT);
         pinMode(echo, INPUT);
--- SR04.hpp
+++ SR04.hpp
@@ -3,15 +3,15 @@
 
 class SR04class {
 public:
-    SR04class() {}
+    SR04class();
 
-    SR04class(uint8_t trig, uint8_t echo) {}
+    SR04class(uint8_t trig, uint8_t echo);
 
-    double distance() {}
+    double distance();
 
-    uint8_t getTrig() {}
+    uint8_t getTrig();
 
-    uint8_t getEcho() {}
+    uint8_t getEcho();
 
 private:
     uint8_t trig, echo;
