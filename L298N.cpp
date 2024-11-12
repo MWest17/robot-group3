c9550e7cfcdf5719153a6901fa8e690852be54a2
M West <mwest49@usf.edu>
2024-11-12 15:59:07 -0500

    Motor functions were implemented and tested

--- L298N.cpp
+++ L298N.cpp
@@ -2,5 +2,76 @@
 
 #include "Arduino.h"
 
-L298NClass::L298NClass(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4) : in1(in1), in2(in2), in3(in3), in4(in4) {}
 
+L298NClass::L298NClass(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4, uint8_t ENA, uint8_t ENB) : in1(in1), in2(in2), in3(in3), in4(in4), ENA(ENA), ENB(ENB) {}
+
+void L298NClass::forward(uint8_t speed) {
+    pinInit();
+
+    //Left Motor Forward
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+
+    //Right Motor Forward
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+
+    //Motor Speed
+    analogWrite(ENA, speed);
+    analogWrite(ENB, speed);
+}
+
+void L298NClass::right(uint8_t speed) {
+    pinInit();
+
+    //Left Motor Forward
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+
+    //Right Motor Forward
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+
+    //Motor Speed
+    analogWrite(ENA, speed);
+    analogWrite(ENB, speed);
+} //Difference in speed of wheels
+//Inverted speed of both wheels
+
+void L298NClass::left(uint8_t speed) {
+    pinInit();
+
+    //Left Motor Forward
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+
+    //Right Motor Forward
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+
+    //Motor Speed
+    analogWrite(ENA, speed);
+    analogWrite(ENB, speed);
+}
+    
+void L298NClass::stop() {
+    pinInit();
+
+    //Left Motor Forward
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+
+    //Right Motor Forward
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+}
+
+void L298NClass::pinInit() {
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
+
+    pinMode(ENA, OUTPUT);
+    pinMode(ENB, OUTPUT);
+}
--- L298N.hpp
+++ L298N.hpp
@@ -2,15 +2,15 @@
 
 class L298NClass {
 public:    
-    L298NClass(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4);
+    L298NClass(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4, uint8_t ENA, uint8_t ENB);
 
-    //motorSpeed(int speedLeft, int speedRight) //negative number is backwards speed on wheel
+    void forward(uint8_t speed);
 
-    /*
-        left(speed, duration)
-        right(speed, duration)  right(speed, degrees)
-        forward(speed) 
-    */
+    void right(uint8_t speed); //Difference in speed of wheels         Speed of a constant turm
+
+    void left(uint8_t speed);
+    
+    void stop();
 
 private:
     //Motor A Pins
@@ -20,7 +20,11 @@ private:
     const uint8_t in3, in4;
 
     //PWM Pins
-    //const uint8_t ENA, ENB;
+    const uint8_t ENA, ENB;
+
+    void pinInit();
+
+   // void motorSpeed(uint8_t speedL, int speedR);
 
 };
 
