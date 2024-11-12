2e1a13ddfc4c6e77ebe0c35356bee56cb5d47d96
M West <mwest49@usf.edu>
2024-11-12 14:50:17 -0500

    Added main robot logic

--- IRSensor.hpp
+++ IRSensor.hpp
@@ -4,10 +4,12 @@
 
 class IRclass {
 public:
+    //Must create object with pin which will be hooked up to output
     IRclass(uint8_t outPin);
 
     uint8_t getPin();
 
+    //Returns true if sensor detects reflected IR Light
     bool isReflecting();
 
 private:
--- robot-group3.ino
+++ robot-group3.ino
@@ -1,11 +1,27 @@
 #include "SR04.hpp"
+#include "IRSensor.hpp"
 
-SR04class SR04_1(12, 11);
+#define IR_OUT_LEFT 8
+#define IR_OUT_RIGHT 9
+
+
+IRclass irL(IR_OUT_LEFT);
+IRclass irR(IR_OUT_RIGHT);
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  delay(500);
-  Serial.println(SR04_1.distance());
+
+  if(irL.isReflecting() && irR.isReflecting()) { //Is on line
+    //Go Straight
+  } else if(irL.isReflecting() && !(irR.isReflecting())) { //Robot right side is off line
+    //Turn Right
+  } else if(!(irL.isReflecting()) && irR.isReflecting()) { //Robot left side is off line
+    //Turn Left
+  } else { //Robot is off line entirely
+    //Stop
+  }
+
 }
